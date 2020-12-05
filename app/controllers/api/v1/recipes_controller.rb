7349e9a131d8a961e012c12340c972f037cd34a8
Alex Henegar <alex.henegar19@gmail.com>
2020-12-05 08:05:22 -0500

    feat: convert to using json api syntax
    ch: [#60]

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,12 +1,21 @@
 module Api
   module V1
     class RecipesController < ApplicationController
+      before_action :set_recipe, only: [:show]
+
       def index
-        render json: { recipes: Recipe.all }
+        @recipes = Recipe.all
+        render json: @recipes
       end
 
       def show
-        render json: { recipe: Recipe.find(params[:id]) }
+        render json: @recipe
+      end
+
+      private
+
+      def set_recipe
+        @recipe = Recipe.find(params[:id])
       end
     end
   end
