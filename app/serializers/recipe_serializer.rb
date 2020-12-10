b2a61a7c1f652bd91abc4ad25015e57dc44a9605
Alex Henegar <alex.henegar19@gmail.com>
2020-12-10 07:30:22 -0500

    feat(create-recipe): initial create recipe
    [ch28]

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,3 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :cook_time
+  attributes :id, :name, :description, :cook_time, :note
 end
--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,1 +1,2 @@
 ActiveModel::Serializer.config.adapter = ActiveModelSerializers::Adapter::JsonApi
+ActiveModel::Serializer.config.key_transform = :camel_lower
