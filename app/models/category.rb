28c7887d38687e2047642937fc91af0c7507efdc
Alex Henegar <alex.henegar19@gmail.com>
2020-12-25 14:18:02 -0500

    feat(recipe-categories): search categories
    > be able to exclude by client_id
    
    [ch75]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
+  include PgSearch::Model
+
   has_many :recipe_categories, primary_key: :client_id
   has_many :recipes, through: :recipe_categories, primary_key: :client_id
+
+  pg_search_scope :search_by_name,
+                  against: :name,
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
