13e1fec82e4746466870f47f4824abebf4fc7919
Alex Henegar <alex.henegar19@gmail.com>
2020-12-27 11:03:31 -0500

    featw(recipe-categories): allow destroy recipe-category relationship
    [ch86]

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   attribute :cook_time, :duration
   has_many :recipe_categories, primary_key: :client_id
-  accepts_nested_attributes_for :recipe_categories
+  accepts_nested_attributes_for :recipe_categories, allow_destroy: true
   has_many :categories, through: :recipe_categories, primary_key: :client_id
   has_many :steps, dependent: :destroy, primary_key: :client_id
   accepts_nested_attributes_for :steps, allow_destroy: true
