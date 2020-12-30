4d82028d4a82f62c441eba069d9791d7ad40907c
Alex Henegar <alex.henegar19@gmail.com>
2020-12-30 16:57:15 -0500

    chore: move rubocop into pre-push hook

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Running pre-push hook"
+./scripts/run-rubocop.sh
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+ echo "Code must be clean before commiting"
+ exit 1
+fi
+
 ./scripts/run-tests.sh
 
 # $? stores exit value of the last command
