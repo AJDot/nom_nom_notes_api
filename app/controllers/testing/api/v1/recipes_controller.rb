01c59e76cd2967e6e0d400e32f6dab2f31eff751
Alex Henegar <alex.henegar19@gmail.com>
2021-01-17 16:09:31 -0500

    test: return 4xx status unless all recipes are created successfully
    [ch95]

--- app/controllers/testing/api/v1/recipes_controller.rb
+++ app/controllers/testing/api/v1/recipes_controller.rb
@@ -6,10 +6,11 @@ module Testing
       # Internal test api for affecting Recipes
       class RecipesController < ApplicationController
         def create
-          recipes = []
-          recipes << Recipe.create(params[:recipe].permit!) if params[:recipe]
-          recipes += Recipe.create(params[:recipes].map(&:permit!)) if params[:recipes]
-          render json: recipes, status: :ok
+          recipes_data = []
+          recipes_data << params[:recipe].permit! if params[:recipe]
+          recipes_data += params[:recipes].map(&:permit!) if params[:recipes]
+          recipes = Recipe.create(recipes_data)
+          render json: recipes, status: recipes.all?(&:valid?) ? :ok : :unprocessable_entity
         end
       end
     end
