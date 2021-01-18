5874b2869a7293a32ef02656fa3357e8fe3dae8b
Alex Henegar <alex.henegar19@gmail.com>
2021-01-18 09:39:05 -0500

    test: add carrierwave testing configs
    [ch95]

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,20 +2,28 @@
 
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
-require 'carrierwave/storage/fog'
-require 'fog/aws'
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Figaro.env.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key: Figaro.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-1',
-
-  }
-  config.fog_directory = Figaro.env.S3_BUCKET_NAME
-  config.fog_public = false
-  config.fog_attributes = {
-    cache_control: "public, max-age=#{365.days.to_i}",
-  }
+if Rails.env.test? # or Rails.env.development?
+  # Fog.mock! if Figaro.env.FOG_MOCK == 'true'
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.root = "#{Rails.root}/tmp" # default is "#{Rails.root}/public"
+  end
+else
+  require 'carrierwave/storage/fog'
+  require 'fog/aws'
+  CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Figaro.env.AWS_ACCESS_KEY_ID,
+      aws_secret_access_key: Figaro.env.AWS_SECRET_ACCESS_KEY,
+      region: Figaro.env.AWS_REGION,
+    }
+    config.fog_directory = Figaro.env.S3_BUCKET_NAME
+    config.fog_public = false
+    config.fog_attributes = {
+      cache_control: "public, max-age=#{365.days.to_i}",
+    }
+  end
 end
