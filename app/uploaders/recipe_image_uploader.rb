00d9f8b623719aa33b9adbce18033e23f6063327
Alex Henegar <alex.henegar19@gmail.com>
2021-01-18 09:39:05 -0500

    test: add carrierwave testing configs
    [ch95]

--- app/uploaders/recipe_image_uploader.rb
+++ app/uploaders/recipe_image_uploader.rb
@@ -2,8 +2,6 @@
 
 # Carrierwave uploader for the image for a recipe
 class RecipeImageUploader < CarrierWave::Uploader::Base
-  storage :fog unless Rails.env.test?
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
