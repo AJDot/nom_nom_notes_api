8445f3249f5a9277553e74764f15ab1e359ac692
Alex Henegar <alex.henegar19@gmail.com>
2022-11-19 00:52:04 -0500

    add amazon for active storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 
 if Rails.env.test? # or Rails.env.development?
-  # Fog.mock! if Figaro.env.FOG_MOCK == 'true'
+  # Fog.mock! if Rails.application.credentials.FOG_MOCK == 'true'
   CarrierWave.configure do |config|
     config.storage = :file
     config.root = Rails.root.join('/tmp') # default is "Rails.root.join('/public')"
@@ -20,7 +20,7 @@ else
       aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
       region: Rails.application.credentials.AWS_REGION,
     }
-    config.fog_directory = Figaro.env.S3_BUCKET_NAME
+    config.fog_directory = Rails.application.credentials.S3_BUCKET_NAME
     config.fog_public = false
     config.fog_attributes = {
       cache_control: "public, max-age=#{365.days.to_i}",
