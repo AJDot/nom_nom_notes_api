8cfc59443c877a36f7aab779ff7279e017224ed6
Alex Henegar <alex.henegar19@gmail.com>
2022-11-28 21:21:54 -0500

    allow using FactoryBot in develop environment

--- spec/factories/ingredient.rb
+++ spec/factories/ingredient.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :ingredient do
     trait :default do
-      description { "#{Faker::Food.unique.measurement} #{Faker::Food.unique.ingredient}" }
+      description { "#{Faker::Food.measurement} #{Faker::Food.unique.ingredient}" }
     end
   end
 end
