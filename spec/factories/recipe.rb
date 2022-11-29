8ae71fcfa0113d14b917dedba88e6e2a9ebe03f1
Alex Henegar <alex.henegar19@gmail.com>
2022-11-28 21:21:54 -0500

    allow using FactoryBot in develop environment

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -3,7 +3,22 @@
 FactoryBot.define do
   factory :recipe do
     trait :default do
-      name { Faker::Food.unique.dish }
+      name { "#{Faker::Food.dish} #{rand(1..1000)}" }
+      description { Faker::Food.description }
+      image { image_file }
+
+      after(:create) do |recipe, _evaluator|
+        FactoryBot.create_list(:ingredient, 5, :default, recipe:)
+      end
     end
   end
+end
+
+def image_file(size: '300x300', format: 'png', background_color: nil, text_color: nil, text: nil)
+  file = Tempfile.new('faker_placeholdit')
+  file.binmode
+  file << Net::HTTP.get(URI(Faker::Placeholdit.image(size:, format:, background_color:, text_color:, text:)))
+  file.close
+
+  ::File.new(file.path)
 end
